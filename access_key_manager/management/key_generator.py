a9ce9646995786304bc06daff8fad4cf0dfe4220
NeonHayford <hayford.lumorvi@gmail.com>
2023-06-19 09:08:46 +0000

    Commit Key generator

--- access_key_manager/management/key_generator.py
+++ access_key_manager/management/key_generator.py
@@ -1,9 +1,8 @@
-import secrets
+from secrets import token_urlsafe
 
 def generate_key(length):
     # random 32 character key length
-    return secrets.token_hex(length)
+    return token_urlsafe(length)
 
-
-
-generate_access_key = generate_key
+generate_access_key = generate_key(32)
+# print(key)
