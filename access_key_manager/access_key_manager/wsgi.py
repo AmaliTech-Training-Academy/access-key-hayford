54dfbc04a01276389cf69e042810683abac5430a
NeonHayford <hayford.lumorvi@gmail.com>
2023-06-22 16:15:58 +0000

    commit change

--- access_key_manager/access_key_manager/wsgi.py
+++ access_key_manager/access_key_manager/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'access_key_manager.settings')
+
+# application = get_wsgi_application()
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'access_key_manager.settings')
+path = '/home/microfocusinc/access-key-hayford/access_key_manager'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'access_key_manager.settings'
+
 
 application = get_wsgi_application()
+
